b4fa34a7bb50b4c3d2f20a5bccb0a5fcad9d0155
ItzBigidy <adrianacosta@Adrians-MBP.attlocal.net>
2023-02-12 23:15:46 -0600

    Added all previous files due to error in old repository

---
+++ script.js
@@ -0,0 +1,89 @@
+// DOM Elements
+const passwordEl = document.getElementById('password');
+const generateEl = document.getElementById('generate');
+const upperCaseEl = document.getElementById('upperCase');
+const lowerCaseEl = document.getElementById('lowerCase');
+const numbersEl = document.getElementById('numbers');
+const specialCharEl = document.getElementById('specialChar');
+const passwordLengthEl = document.getElementById('passwordLength');
+
+const randomFunc = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNumber,
+  specialChar: randomSChar,
+};
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+/* Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+} */
+
+// Add event listener to generate button
+generateEl.addEventListener("click", () =>  {
+  const length = +passwordLengthEl.value;
+  const checkLower = lowerCaseEl.checked;
+  const checkUpper = upperCaseEl.checked;
+  const checkNumbers = numbersEl.checked;
+  const checkSpecialChar = specialCharEl.checked;
+
+  passwordEl.innerText = generatePassword(checkLower, checkUpper, checkNumbers, checkSpecialChar, length)
+})
+
+// Generate Password Function
+function generatePassword(lower, upper, number, specialChar, length) {
+
+  let generatedPassword = '';
+
+  const typesCount = lower + upper + number + specialChar; 
+
+  console.log(typesCount);
+
+  const typesArr = [{lower}, {upper}, {number}, {specialChar}].filter
+  (item => Object.values(item)[0]);
+
+  console.log(typesArr);
+
+  if(typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      console.log(funcName)
+      generatedPassword += randomFunc[funcName]();
+    })
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+  
+  return finalPassword;
+}
+
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function randomSChar() {
+  const symbol = "#$%\"&!'()*+,-./:;<=>?@[]^_`{|}~\\"
+  return symbol[Math.floor(Math.random() * symbol.length)];
+}
+
+
+
